d35808dc28193b8b4f3b5807c85d2b7eb304c1b5
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-01 17:59:33 +0300

    drcc1d1

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6rKp1OHpI6G":
+        Script1();
+        break;
+      case "6EjRdDdKz59":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
